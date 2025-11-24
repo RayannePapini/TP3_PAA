aea17f02e5b4ffbb914372db56e99409a9495670
Rayanne Papini <rayanne.souza@ufv.br>
2025-11-24 13:33:30 -0300

    feat: organizando menu

--- src/menuArq/menuArq.c
+++ src/menuArq/menuArq.c
@@ -110,27 +110,28 @@ void menu(char *textoCriptografado){
         scanf("%d", &opcao);
 
         switch (opcao){
-        case 1:
+            case 1:
 
-            break;
-        case 2:
-            
-            break;
-        case 3:
-            
-            break;
-        case 4:
-            
-            break;
-        case 5:
-            
-            break;
-        case 6:
-            
-            break;        
-        default:
-            continue;
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;        
+            default:
+                continue;
         }
+        
         if(opcao == 6){
             break;
         }
