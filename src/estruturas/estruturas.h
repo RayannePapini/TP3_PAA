22c6c6eafac58c708341bdc4cc6fffa8247ebb97
Tais0357 <taisaparecida0357t@gmail.com>
2025-11-26 21:22:56 -0300

    Add function prototypes for cryptography and analysis

--- src/estruturas/estruturas.h
+++ src/estruturas/estruturas.h
@@ -1,9 +1,22 @@
+#ifndef ESTRUTURAS_H
+#define ESTRUTURAS_H
 
+// Constantes
+#define ALFABETO 26
+#define DESCONHECIDO '_' 
 
 /* Menu/Arquivo */
+char *criptografia(char *texto);
 char *geraArqCripto();
 void menu(char *textoCriptografado);
+void alteraChave(char chave[ALFABETO]);
 
 /* Criptoanálise */
+void inicializaChave(char chave[ALFABETO]);
+void decifraTexto(const char *texto_criptografado, const char *chave, char *texto_decifrado);
+void exibeEstado(const char *texto_criptografado, const char *chave);
+void analiseFrequencia(const char *texto_criptografado, char chave[ALFABETO]);
 
 /* Casamento exato e aproximado */
+
+#endif 
