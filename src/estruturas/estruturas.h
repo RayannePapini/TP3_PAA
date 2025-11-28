9bfdff931c194b4a459b38cb05f28ff7a7584ea5
Tatiana Ribeiro <tatianageralda0357t@gmail.com>
2025-11-28 17:56:13 -0300

    Refactor estruturas.h with new functions and constants

--- src/estruturas/estruturas.h
+++ src/estruturas/estruturas.h
@@ -1,38 +1,35 @@
 #ifndef ESTRUTURAS_H
 #define ESTRUTURAS_H
 
-// Constantes
+#include <stdio.h>
+#include <stdlib.h>
+
 #define ALFABETO 26
 #define DESCONHECIDO '_' 
 
-/* Menu/Arquivo */
+#define COR_VERDE "\033[0;32m"
+#define COR_RESET "\033[0m"
+#define COR_VERMELHO "\033[0;31m"
+
+extern int SHIFT_GLOBAL;
+
 char *criptografia(char *texto);
 char *geraArqCripto();
 void menu(char *textoCriptografado);
 void alteraChave(char chave[ALFABETO]);
 
-/* Criptoanálise */
 void inicializaChave(char chave[ALFABETO]);
 void decifraTexto(const char *texto_criptografado, const char *chave, char *texto_decifrado);
 void exibeEstado(const char *texto_criptografado, const char *chave);
-void analiseFrequencia(const char *texto_criptografado, char chave[ALFABETO]);
-void realizarCasamentoExato(const char *textoCriptografado, char chave[ALFABETO]);
+void analiseFrequencia(const char *texto_criptografado); 
+void analiseCorpus12();
 
-/* Casamento exato e aproximado */
+void altChave(char *textoCriptografado); 
+
+void export(const char *chave); 
+
+void realizarCasamentoExato(const char *textoCriptografado);
+
+void criptografaCorpus12(int shift);
 
 #endif
-
-
-/* Menu/Arquivo */
-char *geraArqCripto();
-void menu(char *textoCriptografado);
-
-/* Criptoanálise */
-
-// Alteração de chave
-void altChave(char *textoCriptografado);
-
-// Finalização/Exportação
-void export(char *textoCriptografado);
-
-/* Casamento exato e aproximado */
