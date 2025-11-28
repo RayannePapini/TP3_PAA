d96e99312f186bf79fdfab8db7a6f6fa1e44485f
Tais0357 <taisaparecida0357t@gmail.com>
2025-11-28 17:58:06 -0300

    Improve user prompts and formatting in main function
    Refactor main function for improved readability and user prompts.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include "estruturas/estruturas.h"
 
-int main(){
+int main() {
     int saida;
-    while(1){
+
+    while (1) {
         printf("Voce ira analisar textos criptografados agora!\n");
-        printf("Caso nao queira continuar no sistema digite 1 para sair!\n");
-        printf("Qualquer outro valor indicará que voce deseja continuar no programa!\n");
+        printf("Digite 1 para sair, ou qualquer outro valor para continuar:\n");
         scanf("%d", &saida);
-        if(saida == 1){
-            break;
-        }
+
+        if (saida == 1) break;
 
         char *textoCriptografado = geraArqCripto();
-        printf("%s\n", textoCriptografado);
+        printf("\nTexto criptografado:\n%s\n", textoCriptografado);
 
         menu(textoCriptografado); 
     }
-    
 
     return 0;
 }
